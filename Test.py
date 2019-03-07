a5133ce31238edb88fb56f9521c7732c04d03209
xuxiaoming <1229717912@qq.com>
2019-03-07 12:54:07 +0800

    Initial commit

---
+++ Test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import os
+
+path = os.path.abspath('..')
+print(os.path.abspath('..'))
+print(path)
---
+++ ship.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import os
+
+import pygame
+
+
+class Ship:
+
+    def __init__(self, screen):
+        """ 初始化飞船并设置其初始位置 """
+        self.screen = screen
+        # 加载飞船图像并获取其外接矩形
+        print("路径：", os.getcwd())
+        print("路径2：", os.path.abspath('..'))
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # 将每一艘飞船放在屏幕底部正中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image,self.rect)
