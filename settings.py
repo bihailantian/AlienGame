2cbf6de52046413cae4bbe00566bd59e0b907360
xuxiaoming <1229717912@qq.com>
2019-03-07 21:06:36 +0800

    调整飞船的速度

--- settings.py
+++ settings.py
@@ -9,4 +9,7 @@ class Settings:
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (230,230,230)
+        self.bg_color = (230, 230, 230)
+
+        # 飞船设置
+        self.ship_speed_factor = 1.5
