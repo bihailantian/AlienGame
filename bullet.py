18dc18984735fcc38b10a755507af22558d95694
xuxiaoming <1229717912@qq.com>
2019-03-08 22:54:50 +0800

    格式化代码

--- bullet.py
+++ bullet.py
@@ -13,26 +13,21 @@ class Bullet(Sprite):
         super(Bullet, self).__init__()
 
         self.screen = screen
-        self.rect = pygame.Rect(0,0,ai_settings.bullet_width,ai_settings.bullet_height)
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
 
-        #存储用小数表示的子弹位置
+        # 存储用小数表示的子弹位置
         self.y = float(ship.rect.y)
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
 
-
     def draw_bullet(self):
         """在屏幕上绘制子弹"""
-        pygame.draw.rect(self.screen,self.color,self.rect)
-
+        pygame.draw.rect(self.screen, self.color, self.rect)
 
     def update(self):
         """更新子弹位置,向上移动"""
         self.y -= self.speed_factor
         self.rect.y = self.y
-
-
-
