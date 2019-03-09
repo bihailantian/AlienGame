35c9cde1de05be5658eef291fc18b6925eef72bd
xuxiaoming <1229717912@qq.com>
2019-03-09 10:28:18 +0800

    创建一群外星人

--- game_functions.py
+++ game_functions.py
@@ -90,7 +90,7 @@ def fire_bullet(bullets, ai_settings, screen, ship):
         bullets.add(new_bullet)
 
 
-def create_fleet(ai_settings, screen, aliens):
+def create_fleet(ai_settings, screen, aliens, ship):
     """
     创建外星人群
 
@@ -101,22 +101,50 @@ def create_fleet(ai_settings, screen, aliens):
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
     number_aliens_x = get_number_aliens_x(ai_settings, alien_width)
+    number_rows = get_number_rows(ai_settings, alien.rect.height, ship.rect.height)
+    # 创建第一群外星人
+    for number_row in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, alien_number, aliens, screen, number_row)
 
-    # 创建第一行外星人
-    for alien_number in range(number_aliens_x):
-        create_alien(ai_settings, alien_number, aliens, screen)
 
-
-def create_alien(ai_settings, alien_number, aliens, screen):
+def create_alien(ai_settings, alien_number, aliens, screen, number_row):
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
+    alien_height = alien.rect.height
     alien.x = alien_width + 2 * alien_width * alien_number
+    alien.y = alien_height + 2 * alien_height * number_row
     alien.rect.x = alien.x
+    alien.rect.y = alien.y
     aliens.add(alien)
 
 
 def get_number_aliens_x(ai_settings, alien_width):
-    """计算每行可容纳多少个外星人"""
+    """
+    计算每行可容纳多少个外星人
+
+    :param ai_settings: 设置
+    :param alien_width: 外星人宽度
+    :return: 每一行的外星人数目
+    """
     available_space_x = ai_settings.screen_width - 2 * alien_width
     number_aliens_x = int(available_space_x / (2 * alien_width))
     return number_aliens_x
+
+
+def get_number_rows(ai_settings, alien_height, ship_height):
+    """
+    计算屏幕可容纳多少行外星人
+
+    :param ai_settings: 设置
+    :param alien_height:  外星人高度
+    :param ship_height:  飞船高度
+    :return: 外星人行数
+    """
+
+    # 屏幕高度减去第一行外星人的上边距（外 星人高度）、飞船的高度以及最初外星人群与飞船的距离（外星人高度的两倍）
+    available_space_y = ai_settings.screen_height - 3 * alien_height - ship_height
+    # 外星人高度 + 间隔高度(alien_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    print("number_rows=", number_rows)
+    return number_rows
